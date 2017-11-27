9aaae92417ee7fde56597c771353c4394bef3422
jessica <jmstuart@umich.edu>
2017-11-27 14:01:55 -0500

    adding everything

--- SI364_HW5.py
+++ SI364_HW5.py
@@ -1,6 +1,10 @@
 ## SI 364
 ## HW5
 
+
+#jessicasi364@gmail.com
+
+
 ## Import statements
 import os
 from flask import Flask, render_template, session, redirect, url_for, flash
@@ -11,6 +15,10 @@ from wtforms.validators import Required
 from flask_sqlalchemy import SQLAlchemy
 
 from flask_migrate import Migrate, MigrateCommand
+
+from flask_mail import Mail, Message
+from threading import Thread
+from werkzeug import secure_filename
 
 # Configure base directory of app
 basedir = os.path.abspath(os.path.dirname(__file__))
@@ -31,12 +39,24 @@ app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 # NOTE: Make sure that you DO NOT write your actual email password in text!!!!
 # NOTE: You will need to use a gmail account to follow the examples in the textbook, and you can create one of those for free, if you want. In THIS application, you should use the username and password from the environment variables, as directed in the textbook. So when WE run your app, we will be using OUR email, not yours.
 
+app.config['MAIL_SERVER'] = 'smtp.googlemail.com'
+app.config['MAIL_PORT'] = 587 #default
+app.config['MAIL_USE_TLS'] = True
+app.config['MAIL_USERNAME'] = os.environ.get('MAIL_USERNAME') 
+app.config['MAIL_PASSWORD'] = os.environ.get('MAIL_PASSWORD')
+app.config['MAIL_SUBJECT_PREFIX'] = '[Hw 5]'
+app.config['MAIL_SENDER'] = 'Admin jessicasi364' # TODO fill in email
+app.config['ADMIN'] = os.environ.get('ADMIN')
+
+
+
 # Set up Flask debug stuff
 manager = Manager(app)
 db = SQLAlchemy(app) # For database use
 migrate = Migrate(app, db) # For database use/updating
 manager.add_command('db', MigrateCommand) # Add migrate
 # TODO: Run commands to create your migrations folder and get ready to create a first migration, as shown in the textbook and in class.
+mail = Mail(app)
 
 ## Set up Shell context so it's easy to use the shell to debug
 def make_shell_context():
@@ -45,6 +65,20 @@ def make_shell_context():
 manager.add_command("shell", Shell(make_context=make_shell_context))
 
 # TODO: Write a send_email function here. (As shown in examples.)
+
+def send_async_email(app, msg):
+    with app.app_context():
+        mail.send(msg)
+
+def send_email(to, subject, template, **kwargs):  
+    msg = Message(app.config['MAIL_SUBJECT_PREFIX'] + ' ' + subject,
+                  sender=app.config['MAIL_SENDER'], recipients=[to])
+    msg.body = render_template(template + '.txt', **kwargs)
+    msg.html = render_template(template + '.html', **kwargs)
+    thr = Thread(target=send_async_email, args=[app, msg]) 
+    thr.start()
+    return thr 
+
 
 #########
 ######### Everything above this line is important/useful setup, not problem-solving.
@@ -73,6 +107,7 @@ class User(db.Model):
     __tablename__ = "users"
     id = db.Column(db.Integer, primary_key=True) ## -- id (Primary Key)
     twitter_username = db.Column(db.String(64), unique=True)
+    email = db.Column(db.String(64))
     def __repr__(self):
         return "{} (ID: {})".format(self.twitter_username,self.id)
 
@@ -93,6 +128,7 @@ class Hashtag(db.Model):
 class TweetForm(FlaskForm):
     text = StringField("What is the text of your tweet? Please separate all hashtags with commas in this case. e.g. 'Yay Python #python, #programming, #awesome' ", validators=[Required()])
     username = StringField("What is your Twitter username?",validators=[Required()])
+    email = StringField (" Enter your email", validators = [Required()])
     submit = SubmitField('Submit')
 
 
@@ -105,12 +141,12 @@ class TweetForm(FlaskForm):
 ## -- Hashtags should be identified by their text (e.g. if there's already a hashtag with that text, return it; otherwise, create it)
 
 # TODO: Edit get_or_create_user (AND get_or_create_tweet -- see below) as necessary to store a user's email as well as their twitter username. The get_or_create_user function should accept an email as input and deal with it appropriately to save it as part of a User row. Each user (from now on) has an email! This should be a small change to how the function currently works.
-def get_or_create_user(db_session, username):
-    user = db_session.query(User).filter_by(twitter_username=username).first()
+def get_or_create_user(db_session, username, email):
+    user = db_session.query(User).filter_by(twitter_username=username, email=email).first()
     if user:
         return user
     else:
-        user = User(twitter_username=username)
+        user = User(twitter_username=username, email =email)
         db_session.add(user)
         db_session.commit()
         return user
@@ -126,12 +162,12 @@ def get_or_create_hashtag(db_session, hashtag_given):
         return hashtag
 
 # TODO: You will need to make changes in this function as well, to address users having emails. See above. What do you need to change to make sure get_or_create_user is *invoked* correctly, including saving an email? Does anything need to change about the input to get_or_create_tweet, and *its* invocations?
-def get_or_create_tweet(db_session, input_text, username):
-    tweet = db_session.query(Tweet).filter_by(text=input_text, user_id=get_or_create_user(db_session, username).id).first()
+def get_or_create_tweet(db_session, input_text, username, email):
+    tweet = db_session.query(Tweet).filter_by(text=input_text, user_id=get_or_create_user(db_session, username, email).id).first()
     if tweet:
         return tweet
     else:
-        user = get_or_create_user(db_session, username)
+        user = get_or_create_user(db_session, username, email)
         tweet = Tweet(text=input_text, user_id=user.id)
         for text in input_text.split(','):
             if "#" in text.strip():
@@ -164,9 +200,10 @@ def index():
     num_tweets = len(tweets)
     form = TweetForm()
     if form.validate_on_submit():
-        if db.session.query(Tweet).filter_by(text=form.text.data, user_id= (get_or_create_user(db.session, form.username.data).id)).first():
+        if db.session.query(Tweet).filter_by(text=form.text.data, user_id= (get_or_create_user(db.session, form.username.data, form.email.data).id)).first():
             flash("You've already saved that tweet by this user!")
-        get_or_create_tweet(db.session, form.text.data, form.username.data)
+        get_or_create_tweet(db.session, form.text.data, form.username.data, form.email.data)
+        send_email(form.email.data, 'New Tweet', 'mail/new_tweet', tweet = form.text.data)
         return redirect(url_for('see_all_tweets'))
     return render_template('index.html', form=form,num_tweets=num_tweets)
 
--- final_project_app_qs.md
+++ final_project_app_qs.md
@@ -2,40 +2,67 @@
 
 ## Overall
 
+
 * **What's a one-two sentence description of what your app will do?**
+
+I will have the user enter an airport code and will then give them the weather to the airport. These will all be stored in a database and will send an email with the highest or lowest the temperature that was recorded when requested. 
 
 ## The Data
 
 * **What data will your app use? From where will you get it? (e.g. scraping a site? what site? -- careful not to run it too much. An API? Which API?)**
 
+I will get data from the FAA REST API that will allow me to acess the weather for U.S. airports. 
+
+https://app.swaggerhub.com/apis/FAA/ASWS/1.1.0#/
+
 * **What data will a user need to enter into a form?**
+
+The user will enter their name and the airport code they want to get the weather and information about. There will also be an option to get an email of the highest and lowest temperature.  
 
 * **How many fields will your form have? What's an example of some data user might enter into it?**
 
+It will have three feilds. One enterting the name of the person looking for the information, the next will be entering the airport code and the other will be the option of sending the highest or lowest temperature that has been recorded to the email. 
+
 * **After a user enters data into the form, what happens? Does that data help a user search for more data? Does that data get saved in a database? Does that determine what already-saved data the user should see?**
 
+The information will be stored and there will be an option to send an email with the information stores. 
+
 * **What models will you have in your application?**
+I will have two models, one of the airport they searched and one of the types of weather. 
 
 * **What fields will each model have?**
+The fields within each models are user names, airport, and weather with the last time it was updated. 
 
 * **What uniqueness constraints will there be on each table? (e.g. can't add a song with the same title as an existing song)**
+You can't add the same airport code. 
 
 * **What relationships will exist between the tables? What's a 1:many relationship between? What about a many:many relationship?**
+It will be a one to many relationship. There is one airport, but you can have many different temperatures. 
+
+Many - many relation: the weather can be rainy ect. can be at many different airports. 
 
 * **How many get_or_create functions will you need? In what order will you invoke them? Which one will need to invoke at least one of the others?**
+I am not exactly sure how many get or create functions I will neeed. 
 
 ## The Pages
 
 * **How many pages (routes) will your application have?**
+It will have at least 3. One will be to enter the information, one will be the list of information already stored, and one will be the option to send an email.  
 
 * **How many different views will a user be able to see, NOT counting errors?**
+There will be multiple views. One will be the log in page entering yoru name which will take you to enter in what airport you want to get weather about. The final page will give an option if they want to recieve an email or not 
+
 
 * **Basically, what will a user see on each page / at each route? Will it change depending on something else -- e.g. they see a form if they haven't submitted anything, but they see a list of things if they have?**
+They will be able to see the weather they requested and any recent updates. They will also be able to choose if they want it uploaded. If they do not submit anything they will receieve a list of all other information they previously entered in an output. 
 
 ## Extras
 
 * **Why might your application send email?**
+It will send emails of updates to the weather for example if my mom is checking the weather for when I am flying.
 
 * **If you plan to have user accounts, what information will be specific to a user account? What can you only see if you're logged in? What will you see if you're not logged in -- anything?**
 
 * **What are your biggest concerns about the process of building this application?**
+
+I do not feel 100% comfortable with data bases yet and am nervous that since you cannot add something with the same name that the application will not work very long.  
